4b32f590b82e123a828798c1ce258a739c0c6462
Faouziath Idrissou <gidrissougado@gmail.com>
2026-03-23 23:40:58 +0100

    new v

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
-  baseURL: baseURL && baseURL.trim().length > 0 ? baseURL : "",
+  baseURL: baseURL && baseURL.trim().length > 0 ? baseURL : "/api",
 });
 
 export default api;
